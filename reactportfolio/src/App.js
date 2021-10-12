50ba538e90d827f0ea38abb56247448495840d4f
EddieH <ehajek@outlook.com>
2021-10-12 10:11:46 -0400

    Start Page

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,13 +1,52 @@
-import './App.css';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+// import Portfolio from './components/Portfolio';
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'About',
+      description: 'About me.'
+    },
+    {
+      name: 'Portfolio',
+      description: 'This is a portfolio of my Coding Projects.'
+    },
+    { 
+      name: 'Resume', 
+      description: 'Take a look and Download a copy.' 
+    }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
- 
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
     </div>
   );
 }
+
 
 export default App;

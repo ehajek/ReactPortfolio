47b694224a5b1af7ed71114e7d2e4027be6030e3
EddieH <ehajek@outlook.com>
2021-10-13 17:29:30 -0400

    re-upload main

---
+++ src/components/Photos/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+const Photos = () => {
+    const photoName = [
+        {
+            id: '1',
+            name: "Note Taker",
+            link: "https://github.com/ehajek/NoteTaker"
+        },
+        {
+            id: '2',
+            name: "Weather Dashboard",
+            link: "https://github.com/ehajek/WeatherDashboard"
+        },
+        {
+            id: '3',
+            name: "Portfolio Generator",
+            link: "https://github.com/ehajek/portfolio-generator"
+        }, {
+            id: '4',
+            name: "Work Day Scheduler",
+            link: "https://github.com/ehajek/WorkDayScheduler"
+        },
+        {
+            id: '5',
+            name: "10Speed",
+            link: "https://github.com/ehajek/10Speed"
+        },
+        {
+            id: '6',
+            name: "Blue 42",
+            link: "https://github.com/DanM1996/blue-42"
+        }
+    ];
+
+    return (
+        <div className="photoBackdrop">
+            <div className="photoContainer">
+                {photoName.map(photo => (
+                    <div className="img__wrap">
+                        <img 
+                        // src={'../../../src/assets/images/1.jpg'}
+                        src={`./assets/images/${photo.id}.jpg`} 
+                        alt={photo.name} 
+                        className="img-fluid photoEdits "
+                        key={photo.id}
+                        ></img>
+                        <p class="img__description">{photo.name} &nbsp; 
+                        <a href={photo.link}>
+                        <FontAwesomeIcon icon={['fab', 'github']} size={['4x']} /> </a> &nbsp;
+                        </p>
+                        <p></p>
+                    </div>
+                ))}
+                <p></p>
+            </div>
+        </div>
+    );
+};
+
+export default Photos;
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Photos from '../Photos';
+
+
+
+
+
+function Portfolio(props) {
+
+  return (
+    <section>
+      <h1 id="portfolio">Portfolio</h1>
+
+      <Photos></Photos>
+    </section>
+  );
+}
+
+export default Portfolio;
